c8db59f10b439e1493c8afad70cad4afd73d9799
Alice <LisaAlisaLis888>
2020-05-02 13:58:53 +0300

    Added config files

---
+++ GamePortal/Nmihay.BlackJack.Logic.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Nmihay.BlackJack.Logic.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
