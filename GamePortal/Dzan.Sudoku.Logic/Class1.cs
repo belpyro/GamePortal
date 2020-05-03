fe1db59e7d4089eaf0418f7aed35a2ba176d6815
Dmitriy Zanko <dmitriy_zanko@mail.ru>
2020-05-04 00:46:16 +0300

    Add projects for Sudoku

---
+++ GamePortal/Dzan.Sudoku.Logic/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dzan.Sudoku.Logic
+{
+    public class Class1
+    {
+    }
+}
