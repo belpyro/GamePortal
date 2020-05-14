68d8bf27d7a7b8e9ed47f4455a130844068c092d
Kiryl Balanovich <balanovichks@gmail.com>
2020-05-14 16:16:50 +0300

    RegisterUserDto created

---
+++ GamePortal/Kbalan.TouchType.Logic/Dto/RegiterUserDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kbalan.TouchType.Logic.Dto
+{
+    public class RegiterUserDto
+    {
+        public int Id { get; set; }
+        public string NickName { get; set; }
+        public string Avatar { get; set; }
+        public string Password { get; set; }
+        public int LevelOfGame { get; set; }
+        public string Role { get; set; }
+        public int MaxSpeedRecord { get; set; }
+        public int NumberOfGamesPlayed { get; set; }
+    }
+}
