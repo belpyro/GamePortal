6816df93561023b22c79ffa3180a1fa99696dacd
Aliaksandr Nadtachayeu <321sand123@gmail.com>
2020-05-17 19:32:43 +0300

    Was created prototype of RESTful WebApi.

---
+++ GamePortal/AliaksNad.Battleship.Logic/Models/User.cs
@@ -0,0 +1,11 @@
+﻿using AliaksNad.Battleship.Logic.DTO;
+
+namespace AliaksNad.Battleship.Logic.Models
+{
+    public class User : LogInDTO
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ GamePortal/GamePortal.Web.Api/Controllers/Battleship/StatisticController.cs
@@ -0,0 +1,39 @@
+﻿using AliaksNad.Battleship.Logic.DB;
+using AliaksNad.Battleship.Logic.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace GamePortal.Web.Api.Controllers.Battleship
+{
+    [RoutePrefix("api/battlesip/statistic")]
+    public class StatisticController : ApiController
+    {
+        protected DataBase _db = new DataBase();
+
+        /// <summary>
+        /// Returns all users statistics.
+        /// </summary>
+        [HttpGet]
+        [Route("")]
+        public IHttpActionResult GetAll()
+        {
+            var stat = _db.GetStatistic().OrderByDescending(x => x.Score);
+            return stat == null ? (IHttpActionResult)NotFound() : Ok(stat);
+        }
+
+        /// <summary>
+        /// Returns all user statistics by id.
+        /// </summary>
+        [HttpGet]
+        [Route("{id}")]
+        public IHttpActionResult GetById(int id)
+        {
+            var stat = _db.GetStatistic().Where(x => x.Id == id);
+            return stat == null ? (IHttpActionResult)NotFound() : Ok(stat);
+        }
+    }
+}
