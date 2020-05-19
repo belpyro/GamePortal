d51b78728782bffab37352fde91b977fe79a9890
vitaly.by <vitaly.by@gmail.com>
2020-05-20 00:51:03 +0300

    * modify api

--- GamePortal/GamePortal.Web.Api/Controllers/Sapper/SapperGameController.cs
+++ GamePortal/GamePortal.Web.Api/Controllers/Sapper/SapperGameController.cs
@@ -7,12 +7,48 @@ using System.Web.Http;
 
 namespace GamePortal.Web.Api.Controllers.Sapper
 {
+    [RoutePrefix("api/SapperGame")]
     public class SapperGameController : ApiController
     {
-        [HttpGet, Route("api/SapperGame/test/result")]
-        public IHttpActionResult TestResultOk()
+
+        /// <summary>
+        /// Get user info by id
+        /// </summary>
+        /// <returns>User info</returns>
+        [HttpGet, Route("users/info/{id}")]
+        public IHttpActionResult UserInfoById(int id)
         {
-            return Ok();
+            return Ok("User info: id = " + id);
+        }
+
+        /// <summary>
+        /// Add user
+        /// </summary>
+        /// <returns>User id</returns>
+        [HttpPost, Route("users/add")]
+        public IHttpActionResult UserAdd(int id)
+        {
+            return Ok("User add: id = " + id);
+        }
+
+        /// <summary>
+        /// Update user
+        /// </summary>
+        /// <returns>User id</returns>
+        [HttpPut, Route("users/update/{id}")]
+        public IHttpActionResult UserUpdate(int id)
+        {
+            return Ok("User update: id = " + id);
+        }
+
+        /// <summary>
+        /// Delete user
+        /// </summary>
+        /// <returns>Ok</returns>
+        [HttpDelete, Route("users/delete/{id}")]
+        public IHttpActionResult UserDelete(int id)
+        {
+            return Ok("User delete: id = " + id);
         }
     }
 }
