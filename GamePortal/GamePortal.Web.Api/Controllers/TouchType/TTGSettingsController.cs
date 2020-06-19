e4514d1ed0340d1250ad441bffa66099eeef5367
Kiryl Balanovich <balanovichks@gmail.com>
2020-06-19 16:11:25 +0300

    async/await add to setting controller and setting service

--- GamePortal/GamePortal.Web.Api/Controllers/TouchType/TTGSettingsController.cs
+++ GamePortal/GamePortal.Web.Api/Controllers/TouchType/TTGSettingsController.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
 
 namespace GamePortal.Web.Api.Controllers.TouchType
@@ -28,22 +29,22 @@ namespace GamePortal.Web.Api.Controllers.TouchType
         //Get All Settings with user
         [HttpGet]
         [Route("")]
-        public IHttpActionResult GetAll()
+        public async Task<IHttpActionResult> GetAllAsync()
         {
-            var result = _settingService.GetAll();
+            var result = await _settingService.GetAllAsync();
             return result.IsSuccess ? Ok(result.Value) : (IHttpActionResult)StatusCode(HttpStatusCode.InternalServerError);
         }
 
         //Get single Setting Info by User Id
         [HttpGet]
         [Route("{id}")]
-        public IHttpActionResult GetById([FromUri]int id)
+        public async Task<IHttpActionResult> GetByIdAsync([FromUri]int id)
         {
             if (id <= 0)
             {
                 return BadRequest("ID must be greater than 0");
             }
-            var result = _settingService.GetById(id);
+            var result = await _settingService.GetByIdAsync(id);
             if (result.IsFailure)
                 return (IHttpActionResult)StatusCode(HttpStatusCode.InternalServerError);
             return result.Value.HasNoValue ? (IHttpActionResult)NotFound() : Ok(result.Value.Value);
@@ -52,9 +53,9 @@ namespace GamePortal.Web.Api.Controllers.TouchType
         //Update single setting by User Id
         [HttpPut]
         [Route("")]
-        public IHttpActionResult Update(int id ,[FromBody]SettingDto model)
+        public async Task<IHttpActionResult> UpdateAsync(int id ,[FromBody]SettingDto model)
         {
-            var result = _settingService.Update(id, model);
+            var result = await _settingService.UpdateAsync(id, model);
             return result.IsSuccess ? Ok($"Settings of user with id {id} updated succesfully!") : (IHttpActionResult)BadRequest(result.Error);
         }
     }
