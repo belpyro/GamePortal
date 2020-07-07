298b123052eb8fa267eb82b5b7acaa76ff408a53
Kiryl Balanovich <balanovichks@gmail.com>
2020-07-07 16:49:37 +0300

    Client progect based on Angular add

---
+++ GamePortal/Kbalan.TouchType.Client/TTGClient/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/main/navbar/navbar.component';
+import { LoginComponent } from './components/main/login/login/login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
