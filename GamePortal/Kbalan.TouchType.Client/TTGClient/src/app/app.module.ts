872f0e7b8eed859c70b26212113bdc5d8564d935
Kiryl Balanovich <balanovichks@gmail.com>
2020-07-08 22:15:52 +0300

    loggin and registration page done

--- GamePortal/Kbalan.TouchType.Client/TTGClient/src/app/app.module.ts
+++ GamePortal/Kbalan.TouchType.Client/TTGClient/src/app/app.module.ts
@@ -2,19 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/main/navbar/navbar.component';
-import { LoginComponent } from './components/main/login/login/login.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/user/login/login.component';
 import { CommonModule } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
-import { RegistrationComponent } from './components/main/registration/registration/registration.component';
+import { RegistrationComponent } from './components/user/registration/registration.component';
+import { AppRoutingModule } from './app-routing.module';
+import { UserComponent } from './components/user/user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     LoginComponent,
-    RegistrationComponent
+    RegistrationComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
@@ -22,7 +25,8 @@ import { RegistrationComponent } from './components/main/registration/registrati
     BrowserAnimationsModule,
     ToastrModule.forRoot({
       progressBar : true
-    })
+    }),
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- GamePortal/Kbalan.TouchType.Client/TTGClient/src/app/components/user/registration/registration.component.ts
+++ GamePortal/Kbalan.TouchType.Client/TTGClient/src/app/components/user/registration/registration.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-registration',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor() { }
+  constructor(private toastr: ToastrService) { }
 
   ngOnInit(): void {
   }
-
+  onSubmit(){
+    this.showError();
+  }
+  showError(){
+    this.toastr.error('Loggin error');
+  }
+  showSuccess() {
+    this.toastr.success('Success');
+  }
 }
+
