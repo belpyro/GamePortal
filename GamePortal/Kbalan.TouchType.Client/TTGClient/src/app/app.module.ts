9c9191717b72b64726b404bb2908c108d9b0cac8
Kiryl Balanovich <balanovichks@gmail.com>
2020-07-08 17:23:04 +0300

    login form done

--- GamePortal/Kbalan.TouchType.Client/TTGClient/src/app/app.module.ts
+++ GamePortal/Kbalan.TouchType.Client/TTGClient/src/app/app.module.ts
@@ -4,15 +4,25 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/main/navbar/navbar.component';
 import { LoginComponent } from './components/main/login/login/login.component';
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+import { RegistrationComponent } from './components/main/registration/registration/registration.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    LoginComponent
+    LoginComponent,
+    RegistrationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot({
+      progressBar : true
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
--- GamePortal/Kbalan.TouchType.Client/TTGClient/src/app/components/main/login/login/login.component.ts
+++ GamePortal/Kbalan.TouchType.Client/TTGClient/src/app/components/main/login/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private toastr: ToastrService) { }
 
   ngOnInit(): void {
+    this.showSuccess();
   }
 
+
+  onSubmit(){
+    this.showError();
+  }
+  showError(){
+    this.toastr.error('Loggin error');
+  }
+  showSuccess() {
+    this.toastr.success('Success');
+  }
 }
