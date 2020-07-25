24bb1de87be16f080bfd1eb446c08d3c0500fc12
Aliaksandr Nadtachayeu <321sand123@gmail.com>
2020-07-25 18:23:32 +0300

    * update secure nuget packages

--- GamePortal/GamePortal.Web.Api/Startup.cs
+++ GamePortal/GamePortal.Web.Api/Startup.cs
@@ -9,31 +9,17 @@ using Owin;
 using NSwag.AspNet.Owin;
 using Ninject.Web.Common.OwinHost;
 using Ninject.Web.WebApi.OwinHost;
-using Ninject.Web.Common;
-using System.Web;
 using Microsoft.AspNet.Identity;
 using Microsoft.Owin.Security.Google;
 using GamePortal.Web.Api.Middleware;
 using Owin.Security.Providers.VKontakte;
-using IdentityServer3.Core.Configuration;
-using IdentityServer3.Core;
-using IdentityServer3.Core.Models;
-using IdentityServer3.Core.Services.InMemory;
-using System.Security.Claims;
-using System.Collections.Generic;
-using System.Security.Cryptography.X509Certificates;
-using System.IO;
-using System.Linq;
 using IdentityServer3.AccessTokenValidation;
-using IdentityServer3.AspNetIdentity;
-using Microsoft.AspNet.Identity.EntityFramework;
 using System.Web.Http.ExceptionHandling;
 using Elmah.Contrib.WebApi;
 using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Cors;
 using System.Web.Cors;
-using Microsoft.AspNet.Identity;
-using GamePortal.Web.Api.Middleware;
+using System.Threading.Tasks;
 
 [assembly: OwinStartup(typeof(GamePortal.Web.Api.Startup))]
 
@@ -53,10 +39,11 @@ namespace GamePortal.Web.Api
             );
 
             var kernel = new StandardKernel(new NinjectSettings { LoadExtensions = true });
-
             kernel.Load(new LogicDIModule(), new TTGDIModule(), new BattleshipLogicDIModule());
 
             config.Services.Replace(typeof(IExceptionLogger), new ElmahExceptionLogger());   // Replace system logger for elmarh
+
+            app.UseSwagger(typeof(Startup).Assembly).UseSwaggerUi3();
 
             FluentValidationModelValidatorProvider.Configure(config, opt =>
             {
@@ -64,6 +51,12 @@ namespace GamePortal.Web.Api
             });
 
             app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie
+            });
+
             app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions
             {
                 ClientId = "719719063561-v05dg5416mu8km1u2filstn03oqj98s4.apps.googleusercontent.com",
@@ -83,47 +76,31 @@ namespace GamePortal.Web.Api
             app.Map("/ttg/login/vk", b => b.Use<TTGVkAuthMiddleWare>());
 
             app.UseIdentityServerBearerTokenAuthentication(new IdentityServerBearerTokenAuthenticationOptions
-                {
-                    Authority = "http://localhost:10000/",
-                    ClientId = "TTGWebClient",
-                    ClientSecret = "secret",
-                    RequireHttps = false,
-                    ValidationMode = ValidationMode.Local,
-                    IssuerName = "http://localhost:10000/",
-                    ValidAudiences = new[] { "http://localhost:10000/resources" }
-                }) ;
-
-            app.UseCookieAuthentication(new CookieAuthenticationOptions
             {
-                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie
+                Authority = "http://localhost:10000/",
+                ClientId = "TTGWebClient",
+                ClientSecret = "secret",
+                RequireHttps = false,
+                ValidationMode = ValidationMode.Local,
+                IssuerName = "http://localhost:10000/",
+                ValidAudiences = new[] { "http://localhost:10000/resources" }
             });
 
             app.UseBattleshipIdentityServer(kernel);
 
-            app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions
+            var provide = new CorsPolicyProvider
             {
-                ClientId = "241000708571-qhb5s5fqe1nin8s33isgvmpfosa0cgpt.apps.googleusercontent.com",
-                ClientSecret = "G3VZRIXRrewqBkFlRKQtLN3o"
-            });
-            var provide = new CorsPolicyProvider();
-            provide.PolicyResolver = ctx => Task.FromResult(new CorsPolicy
-            {
-                AllowAnyHeader = true,
-                AllowAnyMethod = true,
-                AllowAnyOrigin = true
-            }); 
-            app.UseCors(new CorsOptions { PolicyProvider = provide});
+                PolicyResolver = ctx => Task.FromResult(new CorsPolicy
+                {
+                    AllowAnyHeader = true,
+                    AllowAnyMethod = true,
+                    AllowAnyOrigin = true
+                })
+            };
 
-            app.Map("/login/google", x => x.Use<GoogleAuthMiddleware>());
+            app.UseCors(new CorsOptions { PolicyProvider = provide });
 
-            app.UseSwagger(typeof(Startup).Assembly).UseSwaggerUi3()
-                .UseNinjectMiddleware(() => kernel).UseNinjectWebApi(config);          
-        }
-
-        private static X509Certificate2 LoadCertificate()
-        {
-            return new X509Certificate2(
-                Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"bin\Config\idsrv3test.pfx"), "idsrv3test");
+            app.UseNinjectMiddleware(() => kernel).UseNinjectWebApi(config);
         }
     }
 }
