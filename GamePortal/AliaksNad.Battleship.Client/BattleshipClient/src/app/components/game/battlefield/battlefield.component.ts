24d6da8a92d0a31075c9453ab59c2e5a611532cb
Aliaksandr Nadtachayeu <321sand123@gmail.com>
2020-08-01 18:25:03 +0300

    + added ship dead cell

--- GamePortal/AliaksNad.Battleship.Client/BattleshipClient/src/app/components/game/battlefield/battlefield.component.ts
+++ GamePortal/AliaksNad.Battleship.Client/BattleshipClient/src/app/components/game/battlefield/battlefield.component.ts
@@ -7,11 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BattlefieldComponent implements OnInit {
 
-  num: number[] = [1, 2, 3, 4, 5];
+  num: number[][] = [[], []];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  initialMap() {
+
+    let createMap = () => {
+      let a = [];
+      for (let i = 0; i < 10; i++) {
+        for (let e = 0; e < 10; e++) {
+          a.push({ x: i, y: e, hit: Math.random() >= 0.5, hasBoat: Math.random() >= 0.5 });
+        }
+      }
+      return a;
+    }
+
+  }
+
+  visibility: boolean = false
+  // переключаем переменную
+  toggle() {
+    this.visibility = !this.visibility
   }
 
 }
