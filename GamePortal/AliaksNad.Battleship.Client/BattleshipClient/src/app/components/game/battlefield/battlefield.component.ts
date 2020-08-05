30d94ce4dfcdff2a3ea82ae93dcb46a58d8c9d35
Aliaksandr Nadtachayeu <321sand123@gmail.com>
2020-08-06 00:43:43 +0300

    * refactoring and cleanup BattlefieldComponent

--- GamePortal/AliaksNad.Battleship.Client/BattleshipClient/src/app/components/game/battlefield/battlefield.component.ts
+++ GamePortal/AliaksNad.Battleship.Client/BattleshipClient/src/app/components/game/battlefield/battlefield.component.ts
@@ -9,45 +9,25 @@ import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/dr
 
 export class BattlefieldComponent implements OnInit {
 
-  tableColumns = new Array(10);
-  visibility = false;
-  hit = false;
-  ship = false;
   myStyle = 'battlefield-cell__hit';
   red = 'battlefield-cell__miss';
   size = 10;
   arr = new Array();
   sign = new Array();
-  name: number;
   artists = [
     'Artist I - Davido',
     'Artist II - Wizkid',
-    'Artist III - Burna Boy',
-    'Artist IV - Kiss Daniel',
-    'Artist V - Mayorkun',
-    'Artist VI - Mr. Eazi',
-    'Artist VII - Tiwa Savage',
-    'Artist VIII - Blaqbonez',
-    'Artist IX - Banky W',
-    'Artist X - Yemi Alade',
-    'Artist XI - Perruzi',
-    'Artist XII - Seyi Shay',
-    'Artist XIII - Teni'
+    'Artist III - Burna Boy'
   ];
   alteArtists = [
     'Artist 1 — Odunsi',
     'Artist 2 — Nonso',
-    'Artist 3 — Wavy the creator',
-    'Artist 4 — Dwin',
-    'Artist 5 — SDC',
-    'Artist 6 — Teni'
+    'Artist 3 — Wavy the creator'
   ];
+
   constructor() {
   }
 
-  get getStyle() {
-    return this.myStyle.toString;
-  }
   ngOnInit(): void {
     this.foo(this.size);
     console.log('Component "ngOn" initialised!');
@@ -66,6 +46,11 @@ export class BattlefieldComponent implements OnInit {
       }
     }
   }
+
+  push(trIndex, tdIndex): void {
+    this.sign[trIndex][tdIndex] = 'battlefield-cell__hit';
+  }
+
   drop(event: CdkDragDrop<string[]>) {
     if (event.previousContainer !== event.container) {
       transferArrayItem(event.previousContainer.data, event.container.data,
@@ -75,48 +60,4 @@ export class BattlefieldComponent implements OnInit {
     }
   }
 
-  push(trIndex, tdIndex): void {
-    this.sign[trIndex][tdIndex] = 'battlefield-cell__hit';
-  }
-
-  toggle(): void {
-    this.visibility = !this.visibility;
-
-    if (this.red == 'battlefield-cell__hit') {
-      this.red = 'battlefield-cell__miss';
-    } else {
-      this.red = 'battlefield-cell__hit';
-    }
-  }
-
-  checkHit(): void {
-    this.hit = !this.hit;
-  }
-
-  checkShip(): void {
-    this.ship = !this.ship;
-  }
-
-  calculateClasses() {
-    let x = this.ship;
-    let y = this.hit;
-    if (y) {
-      if (x) {
-        return { 'battlefield-cell__hit': true };
-      }
-      return { 'battlefield-cell__miss': true };
-    }
-  }
-
-  getCss() {
-    let x = this.ship;
-    if (x) {
-      return 'battlefield-cell__hit';
-    }
-    return 'battlefield-cell__miss';
-  }
-
-  details(elem: HTMLElement) {
-    elem.className = this.getCss();
-  }
 }
